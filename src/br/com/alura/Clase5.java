db100bc4678297bfab5afd5dbb27012c1ee87ceb
LinaGrajales <linagrajalesg@gmail.com>
2023-03-30 15:27:47 -0500

    Clases 5-7

--- src/br/com/alura/Clase5.java
+++ src/br/com/alura/Clase5.java
@@ -1,7 +1,8 @@
 package br.com.alura;
 
+import br.com.alura.model.Curso;
+
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
